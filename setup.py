bdc666925928b3ee3de947ee91d3b2334adca450
Mike De Smet <mike.desmet@viaa.be>
2019-01-25 23:58:58 +0100

    Prepare for basic structure

--- setup.py
+++ setup.py
@@ -1,13 +1,41 @@
-#!/usr/bin/env python
+import os
 
-from distutils.core import setup
+from setuptools import setup, find_packages
 
+dirname = os.path.dirname(__file__)
+__version__ = open('VERSION').read().strip()
+__author__ = 'EBU'
+# had to call it something...
+__name__ = 'conferatur'
 
-setup(name='Ai-benchmarking-stt',
-      version='0.0.1',
-      description='',
-      author='',
-      author_email='',
-      url='https://github.com/ebu/ai-benchmarking-stt',
-      packages=[],
-      )
+# Auto save to __meta__
+meta_location = os.path.join(dirname, 'src', __name__, '__meta__.py')
+open(meta_location, 'wb').write('''# Automagically created. DO NOT EDIT
+__version__ = '%s'
+__author__ = '%s'
+''' % (__version__, __author__))
+
+setup(
+    name=__name__,
+    url='https://github.com/ebu/ai-benchmarking-stt/',
+    version=__version__,
+    author=__author__,
+    author_email='',
+    description='',
+    long_description=open('README.md').read(),
+    classifiers=[
+        'Programming Language :: Python',
+    ],
+    package_dir={'': 'conferatur'},
+    packages=('conferatur',),
+    install_requires=[
+        'unidecode',
+        'langcodes',
+    ],
+    extras_require={
+        'dev': [
+            'doctest',
+        ],
+    },
+    platforms='any',
+)
