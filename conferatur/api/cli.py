52d3a0b9155df5cd163a864096a7f3ee2287cb5a
MikeSmithEU <github@mikesmith.eu>
2019-02-01 01:31:11 +0100

    Add rudimentary jsonrpc api for normalisation

--- conferatur/api/cli.py
+++ conferatur/api/cli.py
@@ -1,27 +1,38 @@
 """
-Make conferatur available through a jsonapi interface
+Make conferatur available through a rudimentary jsonapi interface
 """
 
-from flask import Flask
-from flask_jsonrpc import JSONRPC
-import argparse
+from jsonrpcserver import method, serve
+from conferatur import __meta__
 
-app = Flask(__name__)
-jsonrpc = JSONRPC(app, '/api', enable_web_browsable_api=True)
+from conferatur.normalisation import core
 
 
-def argparser(parser=None) -> argparse.ArgumentParser:
-    if parser is None:
-        parser = argparse.ArgumentParser()
-    parser.add_argument('--debug', action='store_true', default=False,
-                        help="enable flask debug mode")
+def argparser(parser):
+    parser.add_argument('--host', help='hostname or ip to serve api')
+    parser.add_argument('--port', type=int, help='port used by the server', default=5000)
     return parser
 
 
+@method
+def version():
+    return __meta__.__version__
+
+
+@method
+def normalisation(text, normalisers):
+    assert type(text) is str
+    assert type(normalisers) is list
+    assert len(normalisers)
+
+    composite = core.Composite()
+    for args in normalisers:
+        assert len(args)
+        normaliser = core.name_to_normaliser(args.pop(0))
+        composite.add(normaliser(*args))
+    return composite.normalise(text)
+
+
 def main(parser, args):
-    app.run(debug=args.debug)
+    serve(args.host if args.host else '', port=args.port)
 
-
-if __name__ == '__main__':
-    _parser = argparser()
-    main(_parser, _parser.parse_args())
