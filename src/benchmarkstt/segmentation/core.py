f1d1221a6b743a44b728ab48c851d6b32c9b6e1e
MikeSmithEU <github@mikesmith.eu>
2019-04-17 15:22:00 +0200

    Change segmentation behavior: save segmentedOn separately

--- src/benchmarkstt/segmentation/core.py
+++ src/benchmarkstt/segmentation/core.py
@@ -17,22 +17,14 @@ class Simple(Base):
         self._re = re.compile('(%s)' % (pattern,))
 
     def __iter__(self):
-        start_match = self._re.match(self._text)
         iterable = self._re.split(self._text)
-        if iterable[0] == '':
-            iterable.pop(0)
 
         pos = 0
         length = len(iterable)
 
-        # special case, starts with word break, add it to first word
-        if start_match is not None:
-            matches = iterable[0:3]
-            pos = 3
-            yield Item({"item": matches[1], "type": "word", "@raw": ''.join(matches)})
-
         while pos < length:
             raw = ''.join(iterable[pos:pos+2])
-            if raw != '':
-                yield Item({"item": iterable[pos], "type": "word", "@raw": raw})
+            if raw != '':  # skip possible empty splits (at the end)
+                segmented_on = iterable[pos+1] if pos + 1 < length else None
+                yield Item({"item": iterable[pos], "type": "word", "@raw": raw, "@segmentedOn": segmented_on})
             pos += 2
--- tests/benchmarkstt/test_segmentation_core.py
+++ tests/benchmarkstt/test_segmentation_core.py
@@ -1,24 +1,34 @@
 from benchmarkstt.segmentation import core
 from benchmarkstt.schema import Item
 import pytest
+import re
 
 
 @pytest.mark.parametrize('text,expected', [
-    ('hello    world! how are you doing?!    ', ['hello    ', 'world! ', 'how ', 'are ', 'you ', 'doing?!    ']),
-    ('\nhello    world! how are you doing?!    ', ['\nhello    ', 'world! ', 'how ', 'are ', 'you ', 'doing?!    ']),
+    ('hello    world! how are you doing?!    ',
+     ['hello    ', 'world! ', 'how ', 'are ', 'you ', 'doing?!    ']),
+    ('\nhello    world! how are you doing?!    ',
+     ['\n', 'hello    ', 'world! ', 'how ', 'are ', 'you ', 'doing?!    ']),
     ('single-word', ['single-word']),
-    (' test', [' test']),
-    ('  test', ['  test']),
-    ('  test  ', ['  test  ']),
+    (' test', [' ', 'test']),
+    ('  test', ['  ', 'test']),
+    ('  test  ', ['  ', 'test  ']),
     ('test  ', ['test  ']),
     ('test  B', ['test  ', 'B']),
     ('test  B ', ['test  ', 'B ']),
-    ('\n\n', ['\n\n'])
+    ('\n\n', ['\n\n']),
+    ('A test \n', ['A ', 'test \n']),
+    ('  \r\n Testing, st\n\r\ruff, okaT? \r\n\r\r',
+     ['  \r\n ', 'Testing, ', 'st\n\r\r', 'uff, ', 'okaT? \r\n\r\r'],),
+    ('Candide, "but let us cultivate our\ngarden."\n',
+     ['Candide, ', '"but ', 'let ', 'us ', 'cultivate ', 'our\n', 'garden."\n'])
 ])
 def test_simple(text, expected):
     result = list(core.Simple(text))
     assert ''.join([word['@raw'] for word in result]) == text
     assert len(result) == len(expected)
+
+    segmentation_re = re.compile(r'[\n\t\s]+')
 
     for i in range(0, len(expected)):
         expected_raw = expected[i]
@@ -26,3 +36,6 @@ def test_simple(text, expected):
         assert type(gotten) is Item
         assert expected_raw == gotten['@raw']
         assert expected_raw.strip() == gotten['item']
+
+        # test @segmentatedOn is either None or matches a whitespace
+        assert gotten['@segmentedOn'] is None or segmentation_re.fullmatch(gotten['@segmentedOn']) is not None
