c626b617e26aceba088d03601a936b09968cfa9c
MikeSmithEU <github@mikesmith.eu>
2019-04-26 16:38:16 +0200

    Make flow only support/use a config file for normalization, add support for sections in config files

--- src/benchmarkstt/flow/cli.py
+++ src/benchmarkstt/flow/cli.py
@@ -4,7 +4,9 @@ Do a complete flow of input -> normalization -> segmentation -> metrics
 
 from benchmarkstt.metrics.cli import argparser as args_metrics
 from benchmarkstt.metrics.cli import main as do_metrics
-from benchmarkstt.normalization.cli import args_inputfile, args_logs, args_normalizers, get_normalizer_from_args
+from benchmarkstt.normalization.cli import args_inputfile, args_logs
+from benchmarkstt.normalization import NormalizationComposite, factory
+from benchmarkstt.config import reader
 import argparse
 
 
@@ -12,11 +14,15 @@ def argparser(parser: argparse.ArgumentParser):
     args_logs(parser)
     args_inputfile(parser)
 
+    parser.add_argument('-c', '--config', metavar='file',
+                        help='Specify config file (currently only used for loading normalization rules)')
     args_metrics(parser)
-    args_normalizers(parser)
     return parser
 
 
 def main(parser, args):
-    normalizer = get_normalizer_from_args(args)
+    normalizer = NormalizationComposite()
+    rules = reader(args.config)['normalization']
+    for rule in rules:
+        normalizer.add(factory.create(*rule))
     do_metrics(parser, args, normalizer)
