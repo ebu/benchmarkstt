e15c9396fde3d89f6f28ab394cad6b5800d5c061
MikeSmithEU <github@mikesmith.eu>
2019-05-22 15:32:05 +0200

    additional code coverage

--- tests/benchmarkstt/test_flask.py
+++ tests/benchmarkstt/test_flask.py
@@ -1,12 +1,14 @@
+from benchmarkstt.__meta__ import __version__
 import pytest
 import json
+import sys
 
-from benchmarkstt.__meta__ import __version__
-from benchmarkstt.api.cli import create_app
+pytestmark = pytest.mark.skipif(sys.version_info < (3, 6), reason="requires python3.6 or higher")
 
 
 @pytest.fixture
 def client():
+    from benchmarkstt.api.cli import create_app
     app = create_app()
     client = app.test_client()
     yield client
@@ -20,6 +22,10 @@ def test_general_stuff(client):
 
 @pytest.mark.parametrize('method,params,result', [
     ['version', {}, __version__],
+    ['help', {}, None],
+    ['list.normalization', {}, None],
+    ['normalization.replace', {"text": "Nudge nudge!", "search": "nudge", "replace": "wink"}, {"text": 'Nudge wink!'}],
+    ['metrics.diffcounts', {"ref": "Hello M", "hyp": "Hello W"}, {"equal": 1, "replace": 1, "insert": 0, "delete": 0}],
 ])
 def test_correct_calls(method, params, result, client):
     request = {
@@ -28,9 +34,46 @@ def test_correct_calls(method, params, result, client):
       "params": params,
       "method": method
     }
-    response = {
+    expected_response = {
         "jsonrpc": "2.0",
         "result": result,
         "id": "6gpigblrn4"
     }
-    assert json.loads(client.post('/api', data=json.dumps(request)).data) == response
+
+    response = client.post('/api', data=json.dumps(request))
+    assert response.status_code == 200
+    if result is not None:
+        assert json.loads(response.data) == expected_response
+
+
+@pytest.mark.parametrize('method,params,code,result', [
+    ['doesntexistmethod', {}, 404, {"code": -32601, "message": "Method not found", "data": "doesntexistmethod"}],
+    ['normalization.config',
+     {"text": "He bravely turned his tail and fled", "file": "/etc/hosts", "encoding": "UTF-8"},
+     400,
+     {
+         "code": -32602,
+         "message": "Invalid parameters",
+         "data": "{\"message\": \"Access to unallowed file attempted\", \"field\": \"file\"}"
+     }
+     ],
+])
+def test_error_calls(method, params, code, result, client):
+    request = {
+        "jsonrpc": "2.0",
+        "id": "6gpigblrn4",
+        "params": params,
+        "method": method
+    }
+
+    expected_response = {
+        "jsonrpc": "2.0",
+        "error": result,
+        "id": "6gpigblrn4"
+    }
+
+    response = client.post('/api', data=json.dumps(request))
+    if code is not None:
+        assert response.status_code == code
+    if result is not None:
+        assert json.loads(response.data) == expected_response
