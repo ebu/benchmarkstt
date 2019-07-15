a61d8746d8728a52dc0a0e23d54b339006eb5b3c
EyalLavi <eyal.lavi@bbc.co.uk>
2019-07-15 18:01:06 +0100

    Docs/iss30/document diff (#93)
    * Documented params and diff algo
    
    * Update core.py
    
    * Fixed trailing spaces
    
    * Update core.py
    
    * Moved param tags
    
    * PEP: remove trailing spaces

--- src/benchmarkstt/metrics/core.py
+++ src/benchmarkstt/metrics/core.py
@@ -80,9 +80,17 @@ class WER(Base):
              number of reference words
 
     See: https://en.wikipedia.org/wiki/Word_error_rate
+
+    Insertions, deletions and substitutions are
+    identified using the Hunt–McIlroy diff algorithm.
+    This algorithm is the one used internally by Python.
+    See https://docs.python.org/3/library/difflib.html
+
+    :param mode: WER variant. 'strict' is the default. 'hunt' applies 0.5 weight to insertions and deletions.
+    :param differ_class: For future use.
     """
 
-    # TODO: proper documenting of different modes
+    # WER modes
     MODE_STRICT = 'strict'
     MODE_HUNT = 'hunt'
 
