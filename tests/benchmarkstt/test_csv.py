748c33f17df60b5931f789a6d416d9eda3a7af89
MikeSmithEU <github@mikesmith.eu>
2019-07-17 18:47:22 +0200

    add unit test, make error of file parsing more readable

--- tests/benchmarkstt/test_csv.py
+++ tests/benchmarkstt/test_csv.py
@@ -123,6 +123,8 @@ Normalizer4 "argument with double quote ("")"
     assert _reader('test "stuff\n\t"\n\t  \t  YEs    \t   \n') == \
         [['test', 'stuff\n\t'], ['YEs']]
     assert _reader("\n\n\n\nline5")[0].lineno == 5
+    assert _reader('# Remove XML tags\n"<[^>]+>" " " \n\n# Remove punctuation\n"[,.-]" " "') == \
+        [['<[^>]+>', ' '], ["[,.-]", " "]]
 
 
 def test_exceptions():
