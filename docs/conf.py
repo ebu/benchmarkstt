32589eb2caf73917b83a5f2ee05232a37dd90a8f
MikeSmithEU <github@mikesmith.eu>
2019-08-14 12:43:26 +0200

    documentatio config: use html_css_files

--- docs/conf.py
+++ docs/conf.py
@@ -118,11 +118,9 @@ pygments_style = None
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
-html_context = {
-    'css_files': [
-        '_static/theme_overrides.css',  # override wide tables in RTD theme
-        ],
-     }
+html_css_files = [
+    '_static/theme_overrides.css',
+]
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
