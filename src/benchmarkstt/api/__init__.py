57822d7b95ef3374f7d04908c753404a76beb6ff
MikeSmithEU <github@mikesmith.eu>
2020-08-07 03:23:29 +0200

    Revert "test"
    This reverts commit 0468e60d5abeb2669aaf00a3e51341fad76e5b01.

--- src/benchmarkstt/api/__init__.py
+++ src/benchmarkstt/api/__init__.py
@@ -9,7 +9,5 @@ Structure
     :alt: Package Structure - Click for details
     :target: ../_static/uml/benchmarkstt.api.svg
 
-.. image:: http://g.gravizo.com/source/svg?https%3A%2F%2Fraw.githubusercontent.com%2Febu%2Fbenchmarkstt%2Fuml-schema-sphinx-plantuml-test%2Fdocs%2F_static%2Fuml%2Fbenchmarkstt.api.puml
-
 .. uml:: ../_static/uml/benchmarkstt.api.puml
 """
