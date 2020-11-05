d006c7841c93b8a7011490016c5f69c3306e5dc6
MikeSmithEU <github@mikesmith.eu>
2020-11-05 17:36:45 +0100

    fix WER formula for len(ref) == 0

--- src/benchmarkstt/metrics/core.py
+++ src/benchmarkstt/metrics/core.py
@@ -141,7 +141,7 @@ class WER(Metric):
 
         total = counts.equal + counts.replace + counts.delete
         if total == 0:
-            return 0
+            return 1 if changes else 0
         return changes / total
 
 
